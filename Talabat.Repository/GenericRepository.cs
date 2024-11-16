4d9dcc9c22cc9f6be64a91adc2a97a68467364c6
Hesham Mosaad <heshamprince616@yahoo.com>
2024-11-16 15:22:33 +0200

    Create  interface IGenericRepository creaate Class Generic Repository

---
+++ Talabat.Repository/GenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Repositories.Contract;
+using Talabat.Repository.Data;
+
+namespace Talabat.Repository
+{
+	public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+	{
+		private readonly StoreContext _dbContext;
+
+		public GenericRepository(StoreContext _dbContext)
+		{
+			this._dbContext = _dbContext;
+		} // Ask CLR for Creating Object  from DbContext (Implicitly)
+		public async Task<IEnumerable<T>> GetAllAsync()
+		{
+			return await _dbContext.Set<T>().ToListAsync();
+		}
+
+		public async Task<T?> GetAsync(int id)
+		{
+			return await _dbContext.Set<T>().FindAsync(id);
+		}
+	}
+}
